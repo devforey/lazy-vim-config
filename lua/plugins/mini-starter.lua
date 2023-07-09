ead66b186e87c8bb383d41ffbb64f0241baf0f2d
Mark Christoper Vizcarra <devforey@gmail.com>
2023-07-09 21:15:34 +0800

    Configure mini starter
    Remove Lazygit default mini starter and replace with simplier design.

---
+++ lua/plugins/mini-starter.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "echasnovski/mini.starter",
+    opts = function()
+      local pad = string.rep(" ", 22)
+      local starter = require("mini.starter")
+  --stylua: ignore
+  local config = {
+    evaluate_single = true,
+    header = "Welcome, devforey!",
+    content_hooks = {
+      starter.gen_hook.adding_bullet(pad .. "░ ", false),
+      starter.gen_hook.aligning("center", "center"),
+    },
+  }
+      return config
+    end,
+  },
+}
