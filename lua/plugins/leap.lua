b3b52d9c8d64ee3df2a1772a45c5273890580aa6
Mark Christoper Vizcarra <devforey@gmail.com>
2023-08-10 11:56:07 +0800

    Use leap instead of the default

--- lua/plugins/leap.lua
+++
@@ -1,4 +0,0 @@
-return {
-  -- https://github.com/ggandor/leap.nvim
-  { "ggandor/leap.nvim" },
-}
--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,36 +1,7 @@
 return {
+  { "nvim-neotest/neotest-jest" },
   {
     "nvim-neotest/neotest",
-    dependencies = {
-      "haydenmeade/neotest-jest",
-    },
-    keys = {
-      {
-        "<leader>tl",
-        function()
-          require("neotest").run.run_last()
-        end,
-        desc = "Run Last Test",
-      },
-      {
-        "<leader>tL",
-        function()
-          require("neotest").run.run_last({ strategy = "dap" })
-        end,
-        desc = "Debug Last Test",
-      },
-    },
-    opts = function(_, opts)
-      table.insert(
-        opts.adapters,
-        require("neotest-jest")({
-          jestCommand = "yarn test",
-          env = { CI = true },
-          cwd = function()
-            return vim.fn.getcwd()
-          end,
-        })
-      )
-    end,
+    opts = { adapters = { "neotest-jest" } },
   },
 }
