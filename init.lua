d2f32ccd7c3065e11e5b27f07fc336d18593cb80
Mark Christoper Vizcarra <mark.vizcarra@riaadvisory.com>
2024-02-20 09:04:57 +0800

    Update CopilotChat

--- init.lua
+++ init.lua
@@ -1,16 +1,16 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- vim.g.loaded_python3_provider = 1
--- vim.g.python3_host_prog = "/Users/personal/.pyenv/shims/python3"
---
--- local enable_providers = {
---   "python3_provider",
---   "node_provider",
---   -- and so on
--- }
---
--- for _, plugin in pairs(enable_providers) do
---   vim.g["loaded_" .. plugin] = nil
---   vim.cmd("runtime " .. plugin)
--- end
+vim.g.loaded_python3_provider = 1
+vim.g.python3_host_prog = "/Users/personal/.pyenv/shims/python3"
+
+local enable_providers = {
+  "python3_provider",
+  "node_provider",
+  -- and so on
+}
+
+for _, plugin in pairs(enable_providers) do
+  vim.g["loaded_" .. plugin] = nil
+  vim.cmd("runtime " .. plugin)
+end
