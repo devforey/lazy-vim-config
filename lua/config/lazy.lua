d8d9d18fdf78158f37680b744f63be261f1e8a77
Mark Christoper Vizcarra <mark.vizcarra@riaadvisory.com>
2024-03-25 10:12:56 +0800

    Remove unneeded imports

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,13 +12,9 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.ui.mini-starter" },
-    { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.editor.leap" },
     { import = "lazyvim.plugins.extras.dap.core" },
     -- import/override with your plugins
